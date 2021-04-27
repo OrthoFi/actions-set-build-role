cf6ee4888445de8d579a716353a3591adab91772
Peter Trueblood <Peter.Trueblood@orthofi.com>
2021-04-26 21:08:58 -0600

    feat: set build role

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  try {
+    const productionRole = core.getInput('production-role', {required: true})
+    const devRole = core.getInput('dev-role', {required: true})
+    const environmentName = core.getInput('environment-name', {required: true})
+    let buildRole
+    switch (environmentName) {
+      case 'production':
+      case 'demo':
+      case 'risk':
+      case 'lithium':
+      case 'hydrogen':
+      case 'helium':
+        buildRole = productionRole
+        break
+      default:
+        buildRole = devRole
+        break
+    }
+    core.setOutput('build-role', buildRole)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
